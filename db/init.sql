e45285f7cfc0359ed2b0a6c1f45990d97126909b
kodai <kodaiadvance@icloud.com>
2024-01-12 17:18:44 +0900

    タスクの追加、削除、カレンダーへの反映を行うbackendの作成

--- db/init.sql
+++ db/init.sql
@@ -6,6 +6,22 @@ CREATE TABLE account(
 	password VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE goals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    objective VARCHAR(255) NOT NULL,
+    current_state VARCHAR(255) NOT NULL,
+    deadline DATE NOT NULL,
+    category ENUM('1', '2', '3') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE deadlines (
+    goal_id INT PRIMARY KEY,
+    remaining_days INT NOT NULL,
+    FOREIGN KEY (goal_id) REFERENCES goals(id) ON DELETE CASCADE
+);
+
 INSERT INTO account
 	(name, password)
 VALUES
