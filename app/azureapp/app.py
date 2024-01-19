7dbdc68a09808ca97d5e6fd6066554a731086fe1
noboruhikosaka <gorira.speed@gmail.com>
2024-01-20 01:05:33 +0900

    welcomeの画像追加&API接続

--- app/azureapp/app.py
+++ app/azureapp/app.py
@@ -702,5 +702,24 @@ def auto_select_language():
             conn.close()
 
 
+@app.route('/test')
+def test():
+    client = AzureOpenAI(
+        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+        api_key=os.getenv("AZURE_OPENAI_KEY"),  
+        api_version="2023-05-15"
+    )
+    response = client.chat.completions.create(
+    model="GPT35TURBO", # model = "deployment_name".
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        {"role": "user", "content": "Do other Azure AI services support this too?"}
+    ]
+    )
+    res = response.choices[0].message.content
+    return res
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
